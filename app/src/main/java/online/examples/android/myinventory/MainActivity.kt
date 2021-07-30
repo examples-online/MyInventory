5deb0969a083bf7e586689e70ff3f0415d12ea49
SOS <git@kiumars-ansari.com>
2021-07-30 19:05:09 +0200

    Use fraction in fillMaxWidth

--- app/src/main/java/online/examples/android/myinventory/MainActivity.kt
+++ app/src/main/java/online/examples/android/myinventory/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -38,7 +39,7 @@ class MainActivity : ComponentActivity() {
 fun StartingDisplay() {
     Column(
         modifier = Modifier
-            .fillMaxWidth()
+            .fillMaxWidth(0.6f)
             .height(300.dp)
             .background(Color.LightGray),
         horizontalAlignment = Alignment.CenterHorizontally
